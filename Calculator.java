918756ef56b797c35c5fe09cef23d60ae2888987
Sooraj <rsoorajs@gmail.com>
2021-07-16 11:27:18 +0530

    Slight changes

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,11 @@ public class Calc {
 	
 	Calc(){
 		
-		JFrame jf=new JFrame();
+		JFrame jf=new JFrame("Calculator");
 		jf.setSize(500,600);
+		jf.setLocation(350, 80);
 		jf.setLayout(null);
-		
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton jb = new JButton("0");
 		jb.setBounds(10,480,60,60);
@@ -20,7 +21,9 @@ public class Calc {
 	
 	public static void main(String[] args) {
 		
-		Calc C=new Calc();
+		/*Calc C=new Calc();*/
+		
+		new Calc();
 	}
 
 }
